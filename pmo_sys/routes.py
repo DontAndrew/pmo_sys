c478c555d491140df211d429f0a56b5edf6a2052
DontAndrew <drewluna768@gmail.com>
2020-10-21 06:30:11 +0800

    new page

--- pmo_sys/routes.py
+++ pmo_sys/routes.py
@@ -3,6 +3,11 @@ from flask import render_template, Blueprint
 main = Blueprint("main", __name__)
 
 
+@main.route("/login")
+def login():
+    return render_template("login.html", title="Log in")
+
+
 @main.route("/")
-def hello():
-    return render_template("layout.html")
+def home():
+    return render_template("home.html", title="Home")
